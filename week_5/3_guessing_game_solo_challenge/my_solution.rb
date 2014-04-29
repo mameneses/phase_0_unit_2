8adf749188c355d84fcce3137f34fde126724d23
Matias Meneses <matiasameneses@gmail.com>
2014-04-29 14:03:15 -0700

    Update my_solution.rb

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,53 +1,70 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge myself.
 
 # 2. Pseudocode
 
 # Input: answer  and guess
 # Output: :high, :low or correct answer
 # Steps:
-# 1. 
-#
+# 1. define a method intilize with an argument of 'answer'
+# 2. creater an instance variable'@answer' and set it equal to the argument answer
+# 3. defin a method called guess with an argument of 'guess'.
+# 4. if @guess is less than @answer return symbol ':high' 
+# 5. elsif @guess is more than @answer reurn symbol ':low'
+# 6. elsif @guess is equal to @answer return @answer
+# 7. define a metho 'solved?'
+# 8. 
 
 
 # 3. Initial Solution
+
 
 class GuessingGame
   def initialize(answer)
     @answer = answer
   end
   def guess (guess)
-    if guess < @answer
-      return :high
-    elsif guess > @answer
-      return :low
+    @guess = guess
+    @high = :high
+    @low = :low
+    @correct = :correct
+    if @guess > @answer
+      return @high
+    elsif @guess < @answer
+      return @low
     elsif
-      guess == @answer
-      return @answer
+      @guess == @answer
+      return @correct
     end
   end
   def solved?
-    
+    if @guess == @answer
+      return true
+    end
   end
-  # Make sure you define the other required methods, too
 end
 
 
-
-
 # 4. Refactored Solution
-
-
-
-
-
-
+#
+#
+#
+#
+#
+#
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+game = GuessingGame.new(50)
 
+puts game.guess(10) == :low
 
+puts game.guess(100) == :high
+
+puts game.guess(50) == :corret
+
+puts game.solved? == true 
 
 
 
