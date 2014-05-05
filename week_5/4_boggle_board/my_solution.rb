cacd1fd6c90d4e44b52ddbd2101db6257bd22e6d
Matias Meneses <matiasameneses@gmail.com>
2014-05-04 20:47:40 -0700

    Update my_solution.rb

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -15,10 +15,10 @@ boggle_board = [["b", "r", "a", "e"],
 # 1. create class 'Boggle'
 # 2. define a method 'initialize' and create a parameter of matrix
 # 3. set the instance variable '@board' = to matrix. end.
-# 1. defin a method create_word and that an parameter of an array 'board'  and a second parameter of 'coords'
-# 2. coords should accept two intiger valuses within array and should allow for more pareameters to be passed(*splat)
-# 2. for each 'ccords' use '@board' to designate the corresponding value for the 'coords'
-# 3. join the results for each 'coords' listed with out any spaces 
+# 4. defin a method create_word and that an parameter of an array 'board'  and a second parameter of 'coords'
+# 5. coords should accept two intiger valuses within array and should allow for more pareameters to be passed(*splat)
+# 6. for each 'ccords' use '@board' to designate the corresponding value for the 'coords'
+# 7. join the results for each 'coords' listed with out any spaces 
 
 # Initial Solution
 class Boggle
@@ -41,7 +41,8 @@ puts game.create_word([0,1], [1,1], [2,1], [3,2]) == "rock"
 puts game.create_word([0,1], [1,0], [1,2], [3,3]) == "ride"
 
 # Reflection 
-# 
+# This was a good way to find out where to start with this problem. I figured out that the astrkis (*)
+# is allowing for mulitple arguments to be inputed into the method with out having to define how many.
 
 #-------------------------------------------------------------------------------
 
@@ -60,14 +61,13 @@ end
 
 # Refactored Solution
 
-
 # DRIVER TESTS GO BELOW THIS LINE
 
 puts game.get_row(0) == ["b", "r", "a", "e"]
 
 puts game.get_row(3) == ["t", "a", "k", "e"]
 # Reflection 
-
+#this was very straight forward as the rows are already broken into arrays.
 
 
 #-------------------------------------------------------------------------------
@@ -76,7 +76,7 @@ puts game.get_row(3) == ["t", "a", "k", "e"]
 
 # Pseudocode
 # create a method 'get_col' and set a parameter 'col'
-# for each each array within the larger array call the 'col'th value and trun it unto an array.
+# for each each array within the larger array call the 'col'th value and turn it unto an array.
 
 # Initial Solution
 class Boggle
@@ -95,3 +95,6 @@ puts game.get_col(1) == ["r", "o", "c", "a"]
 puts game.get_col(3) == ["e", "t", "r", "e"]
 
 # Reflection 
+# This was more tricky to understand and required a good amount of research on how to call for a sepecific 
+# index value within nested arrays. I had an ah ha moment when it came to mapping over the the board using their 
+# index values.
